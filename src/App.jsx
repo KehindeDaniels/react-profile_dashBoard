9361d6ec7e4a0dd811396b66eaacfbb5422969e8
kdaniels <kehindedaniels501@gmail.com>
2023-10-30 06:47:29 +0100

    added empy component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <main className="">
+    <section className="profile flex flex-col items-end gap-4 max-w-7xl mx-auto">
+      profile
+    </section>
+
+    <section className="table mt-16">
+      table
+    </section>
+    
+    <footer className="footer">
+      footer
+    </footer>
+  </main>
+);
+
+export default App;
